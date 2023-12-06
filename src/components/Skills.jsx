bb73f29f4376a47821fe2f2d598cce6cb6cf3a4b
chaudhari014 <fatarambhai.k14@gmail.com>
2023-12-06 16:05:06 +0530

    add tech stack

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,30 +8,31 @@ import reactImage from "../assets/react.png";
 import github from "../assets/github.png";
 
 import node from "../assets/node.png";
-import express from "../assets/express.png"
-import mongodb from "../assets/mongodb.png"
-import redis from "../assets/redis.png"
+import express from "../assets/express.png";
+import mongodb from "../assets/mongodb.png";
+import redis from "../assets/redis.png";
 
-import ts from "../assets/ts1.png"  
-import mongoose from "../assets/mongoose.png"
+import ts from "../assets/ts1.png";
+// import mongoose from "../assets/mongoose.png";
 //import chakra from "../assets/chakra.png"
 
-import aws from "../assets/aws.png"
-import mysql from "../assets/mysql.png"
-import sequelize from "../assets/sequelize.png"
+import aws from "../assets/aws.png";
+import mysql from "../assets/mysql.png";
+import PromptEngineering from "../assets/promptengineering.jpg";
+import Angular from "../assets/angular.png";
+import sequelize from "../assets/sequelize.png";
 
-import { Fade } from "react-reveal"
-
+import { Fade } from "react-reveal";
 
 const Skills = () => {
   const techs = [
-  
     {
       id: 11,
       src: mongodb,
       title: "MongoDB",
       style: "shadow-green-500",
-    },  {
+    },
+    {
       id: 5,
       src: node,
       title: "NodeJs",
@@ -44,13 +45,7 @@ const Skills = () => {
       title: "Express",
       style: "shadow-white",
     },
-    {
-      id: 6,
-      src: mongoose,
-      title: "Mongoose",
-      style: "shadow-white",
-    },
-  
+
     {
       id: 12,
       src: redis,
@@ -63,7 +58,7 @@ const Skills = () => {
       title: "React",
       style: "shadow-blue-600",
     },
-  
+
     {
       id: 3,
       src: javascript,
@@ -93,8 +88,7 @@ const Skills = () => {
       src: ts,
       title: "Typescript",
       style: "shadow-blue-500",
-    }
-    ,
+    },
     {
       id: 15,
       src: mysql,
@@ -113,6 +107,18 @@ const Skills = () => {
       title: "Sequelize",
       style: "shadow-green-400",
     },
+    {
+      id: 18,
+      src: PromptEngineering,
+      title: "Prompt Engineering",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 19,
+      src: Angular,
+      title: "Angular",
+      style: "shadow-green-400",
+    },
   ];
 
   return (
@@ -126,23 +132,31 @@ const Skills = () => {
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Tech Stack
           </p>
-          <p className="py-6">These are the technologies I've worked with and used them in my projects.</p>
+          <p className="py-6">
+            These are the technologies I've worked with and used them in my
+            projects.
+          </p>
         </div>
 
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
           {techs.map(({ id, src, title, style }) => (
             <div
               key={id}
               className={`shadow-md hover:scale-110 duration-500 py-2 rounded-lg ${style} skills-card`}
             >
-               <Fade top duration={1000} distance="40px">   <img src={src} alt="" className="w-20 mx-auto skills-card-img" />
+              <Fade top duration={1000} distance="40px">
+                {" "}
+                <img
+                  src={src}
+                  alt=""
+                  className="w-20 mx-auto skills-card-img"
+                />
                 <p className="mt-4 skills-card-name">{title}</p>
-                </Fade>
+              </Fade>
             </div>
           ))}
         </div>
       </div>
-     
     </div>
   );
 };
